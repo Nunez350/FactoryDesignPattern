884892ac3240db61f23be01497e33bd5c895054c
Roy Nunez <roynunez273@gmail.com>
2019-09-03 07:33:16 -0400

    first commit

---
+++ src/com/collabera/FactoryDesign/UFOEnemyShip.java
@@ -0,0 +1,12 @@
+package com.collabera.FactoryDesign;
+public class UFOEnemyShip extends EnemyShip {
+	
+	public UFOEnemyShip(){
+		
+		setName("UFO Enemy Ship");
+		
+		setDamage(20.0);
+		
+	}
+	
+}
